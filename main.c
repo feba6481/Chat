6224523ebf49b2f933af44be31be6c8fa97853bb
Felix Baumann <felix-baumann@hotmail.com>
2016-01-15 19:03:48 +0100

    Dirty, but at least working version.

--- main.c
+++ main.c
@@ -3,20 +3,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+
 void sendMode();
-int calcKey();
-int recieveB(int A);
-void encrypt(char inputString[], int n, int key);
+
+int calcKey(int sock , int mode);
+int recieveB(int A, int mode, int sock);
+void encrypt(char inputString[], int n, int key[], int sock);
 void recieveMode();
-void decrypt(char inputString[], int n, int key);
+void decrypt(char inputString[], int n, int key[]);
 int getP();
 int getG();
+
+void sendKey(int key[], int n, int sock, int mode);
+int establishConnectionToServer(char ip[]);
+int startServer();
 
 int main(void)
 {
     srand(time(NULL));
-
-    //TODO: Chatroom
     int mode = 0;
     printf("MODE: ");
     scanf("%d", &mode);
@@ -31,44 +41,111 @@ int main(void)
 
 void sendMode()
 {
+    int sock = establishConnectionToServer("127.0.0.1");
     char inputString[256];
-    int n = 3;
-    int key = calcKey();
-    printf("Key: %d Bitte zu verschluesselnde Nachricht eingeben: ", key);
+    printf("Bitte zu verschluesselnde Nachricht eingeben: ");
     scanf("%s", inputString);
-    encrypt(inputString, n, key);
+    int n = strlen(inputString);
+    int key[n];
+    char tempi[1];
+    tempi[0] = n;
+    if(!send(sock , tempi, 1, 0))
+        printf("PROBLEMO");
+    sendKey(key, n, sock, 1);
+
+    encrypt(inputString, n, key, sock);
     puts(inputString);
+    close(sock);
 }
 
-int calcKey()
-{
-    int p = getP(), g = getG();
-    int a = rand()%45+1;
-    int A = (int)pow(g, a)%p;
-    int B = recieveB(A);
-    int key = (int)pow(B, a)%p;
-    return key;
-}
-
-int recieveB(int A)
-{
-    //TODO send A
-    printf("%d\n", A);
-    int B = 0; //TODO: get B
-    printf("B: ");
-    scanf("%d", &B);
-
-    return B;
-}
-
-void encrypt(char inputString[], int n, int key)
+void sendKey(int key[], int n, int sock, int mode)
 {
     int i = 0;
     for(i = 0; i<n; i++)
     {
-        inputString[i] = inputString[i] + key; //TODO: actual encryption
-        //TODO send tempi
+        key[i] = calcKey(sock, mode);
+        printf("Key: %d\n", key[i]);
     }
+}
+
+int establishConnectionToServer(char ip[])
+{
+    int sock;
+    struct sockaddr_in server;
+
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr(ip);
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+
+    puts("Connected\n");
+
+    return sock;
+}
+
+
+int calcKey(int sock, int mode)
+{
+    int p = getP(), g = getG();
+    int a = rand()%10+2;
+    int A = (long)pow(g, a)%p;
+    printf("A: %d = %d^%d mod %d\n",A, g, a, p);
+    int B = recieveB(A, mode, sock);
+    int key = (long)pow(B, a)%p;
+    printf("K: %d = %d^%d mod %d\n", key, B, a, p);
+    return key;
+}
+
+int recieveB(int A, int mode, int sock)
+{
+    int B = 0;
+    if(mode)
+    {
+        char tempi[1];
+        tempi[0]= A;
+        send(sock , tempi, strlen(tempi), 0);
+        printf("Sent: %d\n", A);
+        if(!recv(sock , tempi, 2000 , 0))
+            printf("Error Recieving");
+        B = tempi[0];
+        printf("Recieved: %d\n", B);
+    }
+    else
+    {
+        char tempi[1];
+        recv(sock , tempi, 2000 , 0);
+        B = tempi[0];
+        tempi[0]= A;
+        if(!send(sock , tempi, strlen(tempi), 0))
+            printf("Error Sending");
+        printf("Sent: %d\n", A);
+
+    }
+    return B;
+}
+
+void encrypt(char inputString[], int n, int key[], int sock)
+{
+    int i = 0;
+    for(i = 0; i<n; i++)
+    {
+        inputString[i] = inputString[i] + key[i]; //TODO: actual encryption
+    }
+    send(sock , inputString, strlen(inputString), 0);
 }
 
 //SENDMODE END
@@ -77,22 +154,74 @@ void encrypt(char inputString[], int n, int key)
 
 void recieveMode()
 {
+    int sock = startServer();
     char inputString[256];
-    int n = 3;
-    int key = calcKey();
-    printf("Key: %d Zu entschluesselnde Nachricht eingeben: ", key);
-    scanf("%s", inputString);
+    char tempi[1];
+    recv(sock, tempi, 2000, 0);
+    int n = tempi[0];
+    int key[n];
+    sendKey(key, n, sock , 0);
+    if(!recv(sock , inputString, 2000 , 0))
+        printf("Error Recieving");
     decrypt(inputString, n, key);
-    printf("%s", inputString);
+    printf("%s\n", inputString);
+    close(sock);
 }
 
-void decrypt(char inputString[], int n, int key)
+int startServer()
+{
+    int socket_desc , client_sock , c;
+    struct sockaddr_in server , client;
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+
+    return client_sock;
+}
+
+
+void decrypt(char inputString[], int n, int key[])
 {
     int i = 0;
     for(i = 0; i<n; i++)
     {
         //TODO: Recieve B
-        inputString[i] = inputString[i] - key; //TODO: actual encryption
+        inputString[i] = inputString[i] - key[i]; //TODO: actual encryption
 
     }
 }
@@ -109,4 +238,3 @@ int getG()
 {
     return 2;
 }
-
